9ab66837ca2dc42023161859c92299b9a106e4b3
Akshat Singla <akshat946638@gmail.com>
2025-02-16 22:21:20 +0530

    Solution to Find Divisibility by 3 or 5

--- Find Divisibility by 3 or 5@@6788dd63490637cfc44e1ba0/code.c
+++ Find Divisibility by 3 or 5@@6788dd63490637cfc44e1ba0/code.c
@@ -4,12 +4,12 @@ void main(){
     num = int(input())
 
 if num % 3 == 0 and num % 5 == 0:
-    print("Divisible by Both")
+    print("Divisible by Both");
 elif num % 3 == 0:
-    print("Divisible by 3")
+    print("Divisible by 3");
 elif num % 5 == 0:
-    print("Divisible by 5")
+    print("Divisible by 5");
 else:
-    print("Not Divisible")
+    print("Not Divisible");
 
 }
